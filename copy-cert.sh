ed9af0e4c90b845e3b8b8995c042169f25a3ae4f
Poli <raimundophilippe@gmail.com>
2020-10-15 17:13:44 +0200

    Create copy-cert.sh

---
+++ copy-cert.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Automatically copy certs between minio nodes
+#By Poli Systems
+While=1
+
+read -p "Please provide your ssh key location (root only):" SSHKey
+read -p "Enter the machines IP or hostnames seperated by a space :" MachinesIP
+
+NMachines=$(echo "$MachinesIP" | wc -w)
+
+for X in $MachinesIP
+do
+        key=${key}"| "$(ssh -p 22 -i "${SSHKey}"  -x -l root ${X} "cat /etc/minio/certs/public.crt" 2>&1)
+done
+
+export IFS="|"
+for publicCRT in $key; do
+    export IFS=" "
+    echo "This"
+    for X in $MachinesIP
+    do
+        ssh -p 22 -i "${SSHKey}"  -x -l root ${X} "echo ${publicCRT} > /etc/minio/certs/CAs/minio-${While}.crt" 2>&1
+        echo $X
+        echo $While
+        let "While=While+1"
+    done
+    export IFS="|"
+done
